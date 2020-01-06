19d523f52de894c9083b6f089a99c3d62968ae08
AlexHurrell <alex.f.hurrell@gmail.com>
2020-01-07 00:43:39 +0100

    Updates

---
+++ precache-manifest.abfeca240c2ff21d81a1562fa629c322.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a75f185c9b22f5c1f00858ce3578184",
+    "url": "/dictionaryapp/index.html"
+  },
+  {
+    "revision": "27ac4522c88eee5daa9e",
+    "url": "/dictionaryapp/static/css/2.41d2056f.chunk.css"
+  },
+  {
+    "revision": "f633929583f3124f4e47",
+    "url": "/dictionaryapp/static/css/main.1f16807b.chunk.css"
+  },
+  {
+    "revision": "27ac4522c88eee5daa9e",
+    "url": "/dictionaryapp/static/js/2.9f59516d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/dictionaryapp/static/js/2.9f59516d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f633929583f3124f4e47",
+    "url": "/dictionaryapp/static/js/main.3e0e24b9.chunk.js"
+  },
+  {
+    "revision": "1ff372810262d46835d3",
+    "url": "/dictionaryapp/static/js/runtime-main.c6ff31a5.js"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/dictionaryapp/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/dictionaryapp/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/dictionaryapp/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/dictionaryapp/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/dictionaryapp/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/dictionaryapp/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/dictionaryapp/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/dictionaryapp/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/dictionaryapp/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/dictionaryapp/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/dictionaryapp/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/dictionaryapp/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/dictionaryapp/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/dictionaryapp/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/dictionaryapp/static/media/fa-solid-900.f6121be5.woff2"
+  }
+]);
